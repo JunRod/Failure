193aa5f3fa8857dc1f472e7bb12dc79eeae10261
JuNRod <junrodpro@gmail.com>
2023-11-14 13:11:58 -0500

    probando

--- app/favicon.mjs
+++ app/favicon.mjs
@@ -3,7 +3,7 @@ import Image from '@components/image';
 export const runtime = 'edge';
 
 export default async function GenImage() {
-    return await Image('/favicon.png', {
+    return await Image('@componentes/icons/favicon.png', {
         width: 48,
         height: 48,
     });
--- app/twitter-image.mjs
+++ app/twitter-image.mjs
@@ -3,7 +3,7 @@ import Image from '@components/image';
 export const runtime = 'edge';
 
 export default async function GenImage() {
-    return await Image('/large.png', {
+    return await Image('@componentes/icons/large.png', {
         width: 1200,
         height: 630,
     });
