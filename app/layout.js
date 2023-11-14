5586712462422febddf098eb77d069d8d9c49513
JuNRod <junrodpro@gmail.com>
2023-11-13 19:10:04 -0500

    1

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ import Diary from "@styles/diary.module.css";
 const inter = Inter({subsets: ['latin']})
 
 export const metadata = {
-    title: 'ModoGuerra', description: 'Diario con IA',
+    title: 'ToFailure', description: 'ToFailure: Diario con IA',
 }
 
 export default function RootLayout({children}) {
