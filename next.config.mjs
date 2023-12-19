41bdb9cb73a56d7f8694af7a103d52e8879f36b6
JuNRod <junrodpro@gmail.com>
2023-12-19 01:17:54 -0500

    fix mobile

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    env: {NEXT_PUBLIC_OPENAI_API_KEY: process.env.NEXT_PUBLIC_OPENAI_API_KEY}
 }
 export default nextConfig
